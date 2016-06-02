74eb3ddcb3db6c807bb4b02e397654c6d4c9e353
John Smethurst <johnsmethurst@gmail.com>
2016-06-02 12:36:07 +0100

    updating with r functions just written

--- cachematrix.R
+++ cachematrix.R
@@ -4,18 +4,19 @@
 ## Creates a matrix object with a set of methods, to get, set, get the inverse and set the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+     inv <- NULL
+     set <- function(y) {
+          x <<- y
+          inv <<- NULL
+     }
+     get <- function() x
+     setinverse <- function(inverse) inv <<- inverse
+     getinverse <- function() inv
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
+
 
 
 ## For matrix objects created with makeCacheMatrix, this function return the value of the inverse if
@@ -23,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  matrix <- x$get()
-  inv <- solve(matrix, ...)
-  x$setinv(inv)
-  inv
+     inv <- x$getinverse()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+     }
+     data <- x$get()
+     inv <- solve(data, ...)
+     x$setinverse(inv)
+     inv
 }
